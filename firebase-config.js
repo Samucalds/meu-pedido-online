b107a6890de27b9a7d0ad623837f1afd98a14497
Samucalds <Samucalds91@gmail.com>
2025-07-14 23:42:10 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore, collection, getDocs, addDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyArGMJgRxw3qUkQcv6vVur_o921vCbJIFI",
+  authDomain: "meu-pedido-online-c2ff1.firebaseapp.com",
+  projectId: "meu-pedido-online-c2ff1",
+  storageBucket: "meu-pedido-online-c2ff1.appspot.com",
+  messagingSenderId: "949953908074",
+  appId: "1:949953908074:web:8f34ddb9af67a07db1a913",
+  measurementId: "G-B99PGCNDTT"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db, collection, getDocs, addDoc, onSnapshot };
