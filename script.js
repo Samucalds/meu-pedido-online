c1614f5d229aae9f4fa588c57b5bdd4784a8fe8c
Samucalds <Samucalds91@gmail.com>
2025-07-15 21:43:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import { getDatabase, ref, onValue, set, push } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
 
-const firebaseConfig = { /* seu config aqui */ };
+const firebaseConfig = {
+  apiKey: "AIzaSyArGMJgRxw3qUkQcv6vVur_o921vCbJIFI",
+  authDomain: "meu-pedido-online-c2ff1.firebaseapp.com",
+  projectId: "meu-pedido-online-c2ff1",
+  storageBucket: "meu-pedido-online-c2ff1.appspot.com",
+  messagingSenderId: "949953908074",
+  appId: "1:949953908074:web:8f34ddb9af67a07db1a913",
+  measurementId: "G-B99PGCNDTT"
+};
+
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
