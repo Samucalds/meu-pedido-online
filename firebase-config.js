7b7506b28ab52cc04f6d1cc67aecb81fe1b02906
Samucalds <Samucalds91@gmail.com>
2025-08-12 21:26:44 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,18 @@
 // firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+// Configuração do Firebase - substitua com as suas credenciais
 const firebaseConfig = {
-  apiKey: "AIzaSyArGMJgRxw3qUkQcv6vVur_o921vCbJIFI",
+   apiKey: "AIzaSyArGMJgRxw3qUkQcv6vVur_o921vCbJIFI",
   authDomain: "meu-pedido-online-c2ff1.firebaseapp.com",
   projectId: "meu-pedido-online-c2ff1",
-  storageBucket: "meu-pedido-online-c2ff1.appspot.com",
+  storageBucket: "meu-pedido-online-c2ff1.firebasestorage.app",
   messagingSenderId: "949953908074",
   appId: "1:949953908074:web:8f34ddb9af67a07db1a913",
   measurementId: "G-B99PGCNDTT"
 };
 
-// Inicializa o Firebase e exporta o Firestore
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-export { db };
+// Inicializar o Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Ativar Firestore
+const db = firebase.firestore();
